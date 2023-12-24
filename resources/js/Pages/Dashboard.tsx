91487ec4db807cfc7943907f58615d1dd6d43628
daichi.sugiyama <caad12.di2@gmail.com>
2023-12-24 15:16:35 +0900

    LINEで送るボタンを静的に設置 #7

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,9 +1,23 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
 import { PageProps } from '@/types';
-import { Button } from '@chakra-ui/react';
+import { Button, Box } from '@chakra-ui/react';
+import { useEffect } from 'react';
 
 export default function Dashboard({ auth }: PageProps) {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src =
+      'https://www.line-website.com/social-plugins/js/thirdparty/loader.min.js';
+    script.async = true;
+    script.defer = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <AuthenticatedLayout
       user={auth.user}
@@ -22,12 +36,25 @@ export default function Dashboard({ auth }: PageProps) {
           </div>
         </div>
       </div>
-      {/* テスト */}
       <div className="py-12">
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <div className="p-6">
+            <div className="p-6" style={{ display: 'flex' }}>
               <Button colorScheme="red">fuga</Button>
+              <Box mt={1.5} ml={2}>
+                <div
+                  className="line-it-button"
+                  data-lang="ja"
+                  data-type="share-a"
+                  data-env="REAL"
+                  data-url="https://developers.line.biz/ja/docs/line-social-plugins/install-guide/using-line-share-buttons/#using-official-line-icons"
+                  data-color="default"
+                  data-size="large"
+                  data-count="false"
+                  data-ver="3"
+                  style={{ display: 'none' }}
+                ></div>
+              </Box>
             </div>
           </div>
         </div>
