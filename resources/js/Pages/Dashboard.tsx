fb59d28ebc6bc6c70f9fdf9e3f47287aef8370fd
daichi.sugiyama <caad12.di2@gmail.com>
2023-12-27 17:34:17 +0900

    未入力チェックバリデーション追加 #7

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -12,9 +12,12 @@ import {
 } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 import setSendTextByLine from '../Components/Dashboard/Line/setSendText';
+import '../../scss/pages/dashboard.scss';
 
 export default function Dashboard({ auth }: PageProps) {
   const [inputValue, setInputValue] = useState('');
+  // フォーム入力に対するバリデーション エラーありならtrue
+  const [isError, setIsError] = useState(true);
 
   // LINE URLスキームの読み込み
   useEffect(() => {
@@ -32,6 +35,7 @@ export default function Dashboard({ auth }: PageProps) {
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value);
+    setIsError(event.target.value === '' ? true : false);
     setSendTextByLine(event.target.value);
   };
 
@@ -50,8 +54,10 @@ export default function Dashboard({ auth }: PageProps) {
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
             <div className="p-6 text-gray-900">
-              <FormControl>
-                <FormLabel>LINE送信テスト用フォーム</FormLabel>
+              <FormControl isInvalid={isError}>
+                <FormLabel className="form-text">
+                  LINE送信テスト用フォーム
+                </FormLabel>
                 <Input
                   value={inputValue}
                   type="text"
@@ -59,21 +65,29 @@ export default function Dashboard({ auth }: PageProps) {
                   placeholder="送信したいメッセージを入力してください。"
                   onChange={handleInputChange}
                 />
-                <FormHelperText>
-                  LINEでの紹介コード送信テスト用のフォームです
-                </FormHelperText>
+                {isError ? (
+                  <FormErrorMessage>メッセージが未入力です。</FormErrorMessage>
+                ) : (
+                  <FormHelperText>
+                    LINEでの紹介コード送信テスト用のフォームです
+                  </FormHelperText>
+                )}
               </FormControl>
             </div>
           </div>
         </div>
       </div>
-      <div>
+      <div className="Btn-wrap">
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
             <div className="p-6" style={{ display: 'flex' }}>
               <Button colorScheme="red">fuga</Button>
               <Box ml={2}>
-                <a id="line_send_text_icon" href="">
+                <a
+                  id="line_send_text_icon"
+                  href=""
+                  className={isError ? 'disabled' : 'active'}
+                >
                   <img
                     src="http://localhost/img/LINE_Brand_icon.png"
                     alt="LINEロゴ"
