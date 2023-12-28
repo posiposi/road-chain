3db187d0153b02e4223880bfd52cfbc6fd621bb7
daichi.sugiyama <caad12.di2@gmail.com>
2023-12-28 16:46:56 +0900

    テストコード追加 #4

--- app/Http/Requests/Shop/ShopRegisterRequest.php
+++ app/Http/Requests/Shop/ShopRegisterRequest.php
@@ -8,17 +8,26 @@ class ShopRegisterRequest extends FormRequest
 {
     public function authorize()
     {
-        return false;
+        return auth()->check();
     }
 
     public function rules(): array
     {
         return [
-            'shop_name' => ['required', 'string'],
-            'shop_tel' => ['string', 'max:15'],
-            'shop_address' => ['string'],
-            'shop_postal_code' => ['string', 'max:8'],
+            'shop_name' => ['required'],
+            'shop_tel' => ['max:15'],
+            'shop_postal_code' => ['max:8'],
             'shop_email' => ['email'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'shop_name.required' => '店舗名は必須項目です。',
+            'shop_tel.max' => '店舗の電話番号は15文字以下で入力してください。',
+            'shop_postal_code.max' => '店舗の郵便番号は8文字以下で入力してください。',
+            'shop_email.email' => '店舗メールアドレスの形式が不正です。',
         ];
     }
 }
