261da304e8f46ab03a788fed825cb2d49388ceff
daichi.sugiyama <caad12.di2@gmail.com>
2024-01-02 23:34:38 +0900

    Sassバンドル導入 #4

--- resources/js/Components/Dashboard/Shop/RegisterConfirmedModal.tsx
+++ resources/js/Components/Dashboard/Shop/RegisterConfirmedModal.tsx
@@ -9,19 +9,26 @@ import {
   ModalCloseButton,
 } from '@chakra-ui/react';
 import { RegisterConfirmedModalProps } from '@/types';
+import { CheckCircleIcon, WarningIcon } from '@chakra-ui/icons';
 
-const RegisterConfirmedModal: React.FC<RegisterConfirmedModalProps> = ({
+const RegisterConfirmedModal = ({
   isOpen,
   onClose,
-}) => {
+  isSuccess,
+}: RegisterConfirmedModalProps): JSX.Element => {
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>Modal Title</ModalHeader>
+        <ModalHeader>
+          {isSuccess ? <CheckCircleIcon /> : <WarningIcon />}
+          {isSuccess ? '店舗登録完了' : '店舗登録失敗'}
+        </ModalHeader>
         <ModalCloseButton />
-        <ModalBody>
-          <p>Modal body text goes here.</p>
+        <ModalBody className="modal-body-content">
+          {isSuccess
+            ? `店舗登録が完了しました！\n閉じるボタンを押してください。`
+            : `店舗登録に失敗しました。\n時間を空けて再度登録してください。`}
         </ModalBody>
 
         <ModalFooter>
