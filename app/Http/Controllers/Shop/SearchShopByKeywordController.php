5873ff3990634cdb9bdd1752f9165b791b194f6c
daichi.sugiyama <caad12.di2@gmail.com>
2024-01-07 12:03:50 +0900

    エンドポイント設定 #6

---
+++ app/Http/Controllers/Shop/SearchShopByKeywordController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Shop;
+
+use App\Http\Controllers\Controller;
+use Core\src\Shop\Domain\Models\SearchShopKeyword;
+use Illuminate\Http\Request;
+use Core\src\Shop\UseCase\SearchShopByKeyword\SearchShopByKeyword;
+
+class SearchShopByKeywordController extends Controller
+{
+    public function __construct(private SearchShopByKeyword $searchShopByKeyword)
+    {
+    }
+
+    public function __invoke(Request $request)
+    {
+        $shopList = $this->searchShopByKeyword->execute(SearchShopKeyword::from($request->query('searchText')));
+        return response()->json($shopList->items());
+    }
+}
